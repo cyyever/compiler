560aaa50e1fff7b81fb32b5c8e8e11017bb5a889
cyy <cyyever@outlook.com>
2018-11-11 09:41:50 +0800

    format code

--- fuzz_test/lexical_analysis/lexical_analyzer_fuzzing.cpp
+++ fuzz_test/lexical_analysis/lexical_analyzer_fuzzing.cpp
@@ -12,31 +12,32 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 
-	using namespace cyy::computation;
+  using namespace cyy::computation;
   cyy::computation::symbol_string source_code;
 
   for (size_t i = 0; i < Size; i++) {
-	  source_code.push_back(Data[i] % 95 + 32);
+    source_code.push_back(Data[i] % 95 + 32);
   }
   try {
 
-	static cyy::compiler::lexical_analyzer analyzer("common_tokens");
-	static bool inited=false;
+    static cyy::compiler::lexical_analyzer analyzer("common_tokens");
+    static bool inited = false;
 
-	if(!inited) {
-	analyzer.append_pattern(static_cast<symbol_type>(common_token::id),
-			U"[a-zA-Z_][a-zA-Z_0-9]*");
-	analyzer.append_pattern(static_cast<symbol_type>(common_token::digit),
-			U"[0-9]+");
-	analyzer.append_pattern(static_cast<symbol_type>(common_token::whitespace),
-			U"[ \\v\\f\\t\\n\\r\\t]*");
-	analyzer.append_pattern('+', U"\\+");
-	analyzer.append_pattern('*', U"\\*");
-	analyzer.append_pattern('=', U"=");
-	inited=true;
-	}
-	analyzer.set_source_code(  symbol_istringstream (source_code) );
-	analyzer.scan();
+    if (!inited) {
+      analyzer.append_pattern(static_cast<symbol_type>(common_token::id),
+                              U"[a-zA-Z_][a-zA-Z_0-9]*");
+      analyzer.append_pattern(static_cast<symbol_type>(common_token::digit),
+                              U"[0-9]+");
+      analyzer.append_pattern(
+          static_cast<symbol_type>(common_token::whitespace),
+          U"[ \\v\\f\\t\\n\\r\\t]*");
+      analyzer.append_pattern('+', U"\\+");
+      analyzer.append_pattern('*', U"\\*");
+      analyzer.append_pattern('=', U"=");
+      inited = true;
+    }
+    analyzer.set_source_code(symbol_istringstream(source_code));
+    analyzer.scan();
   } catch (const std::invalid_argument &) {
   }
   return 0; // Non-zero return values are reserved for future use.
